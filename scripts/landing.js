cfc502ebb3e7f3996100f815966eb9e6964d283d
Kumeni <samsonkumeni8@gmail.com>
2024-12-13 03:05:10 +0300

    landing images

--- scripts/landing.js
+++ scripts/landing.js
@@ -13,24 +13,24 @@ swiperLanding.on('slideChange', function (event) {
         //slide 1 is active
         for( i=0; i < dynamicTexts.length; i++ ){
             //dynamicTexts[i].style.color = "white !important";
-            document.getElementsByClassName("power-wiz-logo")[0].src = "./assets/icons/power-wiz electro limited logo - blue background.png";
-            dynamicTexts[i].style.color = "white";
+            //document.getElementsByClassName("power-wiz-logo")[0].src = "./assets/icons/power-wiz electro limited logo - blue background.png";
+            //dynamicTexts[i].style.color = "white";
             hambergerMenu.style.backgroundColor = "#3F2B73";
         }
     } else if( event.realIndex == 1) {
         //slide 2 is active
         for( i=0; i < dynamicTexts.length; i++ ){
             //dynamicTexts[i].style.color = "black !important";
-            document.getElementsByClassName("power-wiz-logo")[0].src = "./assets/icons/power-wiz electro limited - yellow background.png";
-            dynamicTexts[i].style.color = "black";
+            //document.getElementsByClassName("power-wiz-logo")[0].src = "./assets/icons/power-wiz electro limited - yellow background.png";
+            //dynamicTexts[i].style.color = "black";
             hambergerMenu.style.backgroundColor = "#FCC22C";
         }
     } else if( event.realIndex == 2) {
         //slide 3 is active
         for( i=0; i < dynamicTexts.length; i++ ){
             //dynamicTexts[i].style.color = "white !important";
-            document.getElementsByClassName("power-wiz-logo")[0].src = "./assets/icons/power-wiz electro limited - green background.png";
-            dynamicTexts[i].style.color = "white";
+            //document.getElementsByClassName("power-wiz-logo")[0].src = "./assets/icons/power-wiz electro limited - green background.png";
+            //dynamicTexts[i].style.color = "white";
             hambergerMenu.style.backgroundColor = "#089547";
         }
     }
--- scripts/swiper/initialize-swiper.js
+++ scripts/swiper/initialize-swiper.js
@@ -97,3 +97,53 @@ let swiperLanding = new Swiper('.swiper-landing', {
   //     },
   // }
 });
+
+let swiperLandingNew = new Swiper('.swiper-landing-new', {
+  // Optional parameters
+  speed:500,
+  spaceBetween:10,
+  slidesPerView:1,
+  direction: 'horizontal',
+  loop: true,
+  effect: "fade",
+  // If we need pagination
+  allowTouchMove:true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+
+  autoplay:{
+    delay:3000,
+    disableOnInteraction:false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  // Responsive breakpoints
+  // breakpoints: {
+  //     //when window width is <=425px
+  //     0:{
+  //       slidesPerView:1,
+  //     },
+  //     // when window width is >= 580px
+  //     580: {
+  //         slidesPerView: 2,
+  //         //spaceBetween: 20
+  //     },
+  //     // when window width is >= 1088px
+  //     1088: {
+  //         slidesPerView: 3,
+  //         spaceBetween: 30
+  //     },
+  // }
+});
